00c55dad48ae5bd2d1107a0901ca72d0a56429b1
Ramazan <ospanr4@gmail.com>
2025-02-06 20:05:40 +0500

    add gpu off flag

--- test.py
+++ test.py
@@ -21,6 +21,10 @@ def fetch_magnum_page(url):
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-webgl")  # Отключение WebGL
+    options.add_argument("--use-gl=swiftshader")  # Использование SwiftShader
+    options.add_argument("--log-level=3")  # Уменьшение логов
 
     driver = webdriver.Chrome(service=service, options=options)
 
